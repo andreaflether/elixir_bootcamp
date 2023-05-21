e56a1d48102742635da251b74af66b6d98041a3f
Andréa Alencar <andreaflether@gmail.com>
2023-05-21 19:45:13 -0300

    Moving .git

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Cards.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
     ]
   end
 end
