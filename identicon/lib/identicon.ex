9300a670afa7e0b21cc6f0a3f3ee209b970b7c87
Andréa Alencar <andreaflether@gmail.com>
2023-05-23 21:59:23 -0300

    [44] Building the grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
@@ -14,5 +15,10 @@ defmodule Identicon do
 
   def pick_color(%Identicon.Image{ hex: [r, g, b | _tail ] } = image) do
     %Identicon.Image{ image | color: { r, g, b } }
+  end
+
+  def build_grid(%Identicon.Image{ hex: hex } = _image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
   end
 end
